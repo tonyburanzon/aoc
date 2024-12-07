be137a968ee23527325c417bdd5fe2b44ecb70fd
Tony Buranzon <tony@buranzon.com>
2024-12-06 22:09:29 -0800

    Rewrite part 1

--- 2024/day06/part1.cpp
+++ 2024/day06/part1.cpp
@@ -9,6 +9,7 @@ public:
   int row;
   int col;
   char dir;
+  Guard() : row(0), col(0), dir('^') {};
   Guard(int row, int col, char dir) :
     row(row), col(col), dir(dir)
   {
@@ -72,68 +73,129 @@ public:
 
 };
 
+class Game 
+{
+public:
+  vector<string> start_grid;
+  int start_row;
+  int start_col;
+  char start_dir;
+  int rowSize;
+  int colSize;
+  vector<string> grid;
+  Guard guard;
+  vector<pair<int, int>> found_positions;
+
+
+
+  void fillStartPositions() 
+  {
+    for(int row = 0; row < this->start_grid.size(); row++)
+    {
+      for(int col = 0; col < this->start_grid[row].length(); col++)
+      {
+        if(
+          this->start_grid[row][col] == '^' ||
+          this->start_grid[row][col] == '>' ||
+          this->start_grid[row][col] == 'v' ||
+          this->start_grid[row][col] == '<' 
+        )
+        {
+          this->start_dir = this->start_grid[row][col];
+          this->start_row = row;
+          this->start_col = col;
+          return;
+        }
+      }
+    }
+  }
+
+  void redrawGrid()
+  {
+    this->grid.clear();
+    for(int row = 0; row < this->start_grid.size(); row++)
+    {
+      string new_input = "";
+      for(int col = 0; col < this->start_grid[row].length(); col++)
+      {
+        new_input += this->start_grid[row][col];
+      }
+      this->grid.push_back(new_input);
+    }
+  }
+
+  void init() 
+  {
+    string input;
+    while(getline(cin, input))
+    {
+      this->start_grid.push_back(input);
+    }
+    this->fillStartPositions();
+    this->redrawGrid();
+    this->guard.row = this->start_row;
+    this->guard.col = this->start_col;
+    this->guard.dir = this->start_dir;
+  }
+
+  Game(): start_dir('|') {
+    this->init();
+    this->rowSize = this->start_grid.size();
+    this->colSize = this->start_grid[0].length();
+  };
+
+  bool isPosOOB(pair<int, int> pos)
+  {
+    return pos.first < 0 ||
+    pos.first >= this->rowSize || 
+    pos.second < 0 ||
+    pos.second >= this->colSize;
+  }
+
+  bool isNextPosOOB()
+  {
+    return this->isPosOOB(this->guard.getNextPos());
+  }
+
+  bool isNextPosBlocked(pair<int, int> pos)
+  {
+    return this->grid[pos.first][pos.second] == '#';
+  }
+
+  void moveGuard()
+  {
+    pair<int, int> next_pos = this->guard.getNextPos();
+    while(this->isNextPosBlocked(next_pos))
+    {
+      if(this->isPosOOB(next_pos))
+         return;
+      this->guard.changeDir();
+      next_pos = this->guard.getNextPos();
+    }
+    this->grid[next_pos.first][next_pos.second] = 'X';
+    this->guard.move();
+  }
+
+  int countSpaces()
+  {
+    int count = 0;
+    for(int row = 0; row < this->rowSize; row++)
+      for(int col = 0; col < this->colSize; col++)
+        if(this->grid[row][col] == 'X')
+          count++;
+    return count;
+  }
+
+
+};
+
 int main()
 {
-  vector<string> grid;
-  string input;
-  while(getline(cin, input))
-    grid.push_back(input);
-
-  pair<int, int> pos;
-  char start = '|';
-  for(int row = 0; row < grid.size(); row++)
+  Game game;
+  
+  while(!game.isNextPosOOB()) 
   {
-    for(int col = 0; col < grid[row].length(); col++)
-    {
-      if(
-        grid[row][col] == '^' ||
-        grid[row][col] == '>' ||
-        grid[row][col] == 'v' ||
-        grid[row][col] == '<'
-      )
-      {
-        pos.first = row;
-        pos.second = col;
-        start = grid[row][col];
-        break;
-      }
-      if(start != '|')
-        break;
-    }
-  }
-  int rowSize = grid.size();
-  int colSize = grid[0].length();
-
-  Guard g(pos.first, pos.second, start);
-  pair<int, int> next_pos = g.getNextPos();
-  while(
-    next_pos.first >= 0 &&
-    next_pos.first < rowSize &&
-    next_pos.second >= 0 &&
-    next_pos.second < colSize
-  )
-  {
-    grid[g.row][g.col] = 'X';
-    while(grid[next_pos.first][next_pos.second] == '#')
-    {
-      g.changeDir();
-      next_pos = g.getNextPos();
-    }
-    g.move();
-    next_pos = g.getNextPos();
-  }
-
-  int count = 1;
-  for(int row = 0; row < rowSize; row++)
-  {
-    for(int col = 0; col < colSize; col++)
-    {
-      cout << grid[row][col];
-      if(grid[row][col] == 'X')
-        count++;
-    }
-    cout << endl;
-  }
-
-  cout << "Count: " << count << endl;
+    game.moveGuard();
+  };
+  cout << game.countSpaces() << endl;
 }
