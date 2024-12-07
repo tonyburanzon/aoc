310e8e53db898c7c67652bf8997a8141c463a930
Tony Buranzon <tony@buranzon.com>
2024-12-06 23:13:59 -0800

    Progress on part 2.

---
+++ 2024/day06/part2.cpp
@@ -0,0 +1,297 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Guard {
+public:
+  int row;
+  int col;
+  char dir;
+  Guard() : row(0), col(0), dir('^') {};
+  Guard(int row, int col, char dir) :
+    row(row), col(col), dir(dir)
+  {
+  };
+
+
+  void changeDir() 
+  {
+    switch(dir) 
+    {
+      case '^': {
+        dir = '>';
+        break;
+      }
+      case '>': {
+        dir = 'v';
+        break;
+      }
+      case 'v': {
+        dir = '<';
+        break;
+      }
+      case '<': {
+        dir = '^';
+        break;
+      }
+    }
+  }
+
+  char getRotatedDir() 
+  {
+    switch(this->dir) 
+    {
+      case '^': return '>';
+      case '>': return'v';
+      case 'v':return '<';
+      case '<': return '^';
+    }
+  }
+
+  pair<int, int> getNextPos()
+  {
+    switch(dir) 
+    {
+      case '^': return  make_pair(row-1, col);
+      case '>': return make_pair(row, col+1);
+      case 'v': return make_pair(row+1, col);
+      case '<': return make_pair(row, col-1);
+    }
+  }
+
+  pair<int, int> getStraightBlockPos()
+  {
+    switch(dir) 
+    {
+      case '^': return  make_pair(row-2, col);
+      case '>': return make_pair(row, col+2);
+      case 'v': return make_pair(row+2, col);
+      case '<': return make_pair(row, col-2);
+    }
+  }
+
+  pair<int, int> getNextPos(char dir)
+  {
+    switch(dir) 
+    {
+      case '^': return  make_pair(row-1, col);
+      case '>': return make_pair(row, col+1);
+      case 'v': return make_pair(row+1, col);
+      case '<': return make_pair(row, col-1);
+    }
+  }
+  
+
+  pair<int, int> getRotatedPos()
+  {
+    char rotated_dir = this->getRotatedDir();
+    return this->getNextPos(rotated_dir);
+  }
+
+
+  void move() 
+  {
+    switch(dir) {
+      case '^': {
+        this->row--;
+        break;
+      }
+      case '>': {
+        this->col++;
+        break;
+      }
+      case 'v': {
+        this->row++;
+        break;
+      }
+      case '<': {
+        this->col--;
+        break;
+      }
+    };
+  }
+
+};
+
+class Game 
+{
+public:
+  vector<string> start_grid;
+  int start_row;
+  int start_col;
+  char start_dir;
+  int rowSize;
+  int colSize;
+  vector<string> grid;
+  Guard guard;
+  vector<pair<int, int>> found_positions;
+
+
+
+  void fillStartPositions() 
+  {
+    for(int row = 0; row < this->start_grid.size(); row++)
+    {
+      for(int col = 0; col < this->start_grid[row].length(); col++)
+      {
+        if(
+          this->start_grid[row][col] == '^' ||
+          this->start_grid[row][col] == '>' ||
+          this->start_grid[row][col] == 'v' ||
+          this->start_grid[row][col] == '<' 
+        )
+        {
+          this->start_dir = this->start_grid[row][col];
+          this->start_row = row;
+          this->start_col = col;
+          return;
+        }
+      }
+    }
+  }
+
+  void redrawGrid()
+  {
+    this->grid.clear();
+    for(int row = 0; row < this->start_grid.size(); row++)
+    {
+      string new_input = "";
+      for(int col = 0; col < this->start_grid[row].length(); col++)
+      {
+        new_input += this->start_grid[row][col];
+      }
+      this->grid.push_back(new_input);
+    }
+  }
+
+  void reset()
+  {
+    this->redrawGrid();
+    this->guard.row = this->start_row;
+    this->guard.col = this->start_col;
+    this->guard.dir = this->start_dir;
+  }
+
+  void init() 
+  {
+    string input;
+    while(getline(cin, input))
+    {
+      this->start_grid.push_back(input);
+    }
+    this->fillStartPositions();
+    this->reset();
+  }
+
+
+  Game(): start_dir('|') {
+    this->init();
+    this->rowSize = this->start_grid.size();
+    this->colSize = this->start_grid[0].length();
+  };
+
+  bool isPosOOB(pair<int, int> pos)
+  {
+    return pos.first < 0 ||
+    pos.first >= this->rowSize || 
+    pos.second < 0 ||
+    pos.second >= this->colSize;
+  }
+
+  bool isNextPosOOB()
+  {
+    return this->isPosOOB(this->guard.getNextPos());
+  }
+
+  bool isNextPosBlocked(pair<int, int> pos)
+  {
+    return this->grid[pos.first][pos.second] == '#';
+  }
+
+  void moveGuard()
+  {
+    pair<int, int> next_pos = this->guard.getNextPos();
+    while(this->isNextPosBlocked(next_pos))
+    {
+      if(this->isPosOOB(next_pos))
+         return;
+      this->guard.changeDir();
+      next_pos = this->guard.getNextPos();
+    }
+    this->grid[next_pos.first][next_pos.second] = this->guard.dir;
+    this->guard.move();
+  }
+
+  bool willNextPosCauseLoop()
+  {
+    pair<int, int> next_pos = this->guard.getNextPos();
+    return this->grid[next_pos.first][next_pos.second] == this->guard.getRotatedDir();
+  }
+
+  bool willRotatedPosCauseLoop()
+  {
+    pair<int, int> rotated_pos = this->guard.getRotatedPos();
+    return this->grid[rotated_pos.first][rotated_pos.second] == this->guard.getRotatedDir();
+  }
+
+  bool hasPosBeenFound(pair<int, int> pos)
+  {
+    for(int i = 0; i < this->found_positions.size(); i++)
+      if(this->found_positions[i].first == pos.first &&
+        this->found_positions[i].second == pos.second)
+        return true;
+    return false;
+  }
+
+  void printFound()
+  {
+    for(int i = 0; i < this->found_positions.size(); i++)
+      cout << this->found_positions[i].first << " " << this->found_positions[i].second << endl;
+  }
+
+  void printGrid()
+  {
+    int count = 0;
+    for(int row = 0; row < this->rowSize; row++)
+    {
+      for(int col = 0; col < this->colSize; col++)
+        cout << this->grid[row][col];
+      cout << endl;
+    }
+    cout << endl;
+  }
+
+
+};
+
+int main()
+{
+  Game game;
+  
+  while(!game.isNextPosOOB()) 
+  {
+    if(game.willNextPosCauseLoop() && !game.hasPosBeenFound(game.guard.getStraightBlockPos()))
+    {
+      cout << "Straight Block" << endl;
+      game.found_positions.push_back(game.guard.getStraightBlockPos());
+      game.printGrid();
+      game.reset();
+      continue;
+    }
+
+    if(game.willRotatedPosCauseLoop() && !game.hasPosBeenFound(game.guard.getNextPos()))
+    {
+      cout << "Rot Block" << endl;
+      game.found_positions.push_back(game.guard.getNextPos());
+      game.printGrid();
+      game.reset();
+      continue;
+    }
+
+    game.moveGuard();
+  };
+  game.printFound();
+  cout << game.found_positions.size() << endl;
+}
