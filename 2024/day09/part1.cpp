f347a52bc9ed01452b9cf321e08043ec7a3cd062
Tony Buranzon <tony@buranzon.com>
2025-11-14 23:52:23 +0000

    WIP: checksum too low

--- 2024/day09/part1.cpp
+++ 2024/day09/part1.cpp
@@ -7,21 +7,16 @@ using namespace std;
 
 void read_file(string& str)
 {
-  ifstream file("test_input");
+  ifstream file("input");
   getline(file, str);
 }
-
-int main() 
+void write_raw_disk(string& disk_map, string& raw_disk)
 {
-  string disk_map = "";
-  read_file(disk_map);
-   
-  string raw_disk = "";
   int id = 0;
 
   for(int i = 0; i < disk_map.length(); i += 2)
   {
-    int file_size = disk_map[i] = '0';
+    int file_size = disk_map[i] - '0';
     int blank_space = i+1 < disk_map.length() ? disk_map[i+1] - '0' : 0;  
 
     for(int j = 0; j < file_size; j++)
@@ -32,8 +27,53 @@ int main()
 
     id++;
   }
+}
 
-  cout << raw_disk << endl;
+void defrag(string& disk)
+{
+  int leftIndx = 0;
+  int rightIndx = disk.length() - 1;
 
+  while( leftIndx < rightIndx )
+  {
+    while( leftIndx < rightIndx && disk[leftIndx] != '.' )
+      leftIndx++;
+    
+    while ( rightIndx > leftIndx && disk[rightIndx] == '.' )
+      rightIndx--;
+
+    disk[leftIndx] = disk[rightIndx];
+    disk[rightIndx] = '.';
+    //leftIndx++; rightIndx--;
+  }
+}
+
+unsigned long long int find_checksum(string& disk)
+{
+  unsigned long long int checksum = 0;
+  for(unsigned long long int i = 0; i < disk.length(); i++)
+  {
+    if(disk[i] == '.')
+      continue;
+
+    checksum += (disk[i] - '0') * i;
+  }
+
+  return checksum;
+};
+
+int main() 
+{
+  string disk_map = "";
+  read_file(disk_map);
+   
+  string raw_disk = "";
+  write_raw_disk(disk_map , raw_disk);
+
+  defrag(raw_disk);
+
+  //cout << raw_disk << endl << endl;
+
+  cout << find_checksum(raw_disk) << endl;
   return 0;
 }
