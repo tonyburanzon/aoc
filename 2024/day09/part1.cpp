be145a2b8c07154376912555a7a43a8ebca8f458
Tony Buranzon <tony@buranzon.com>
2025-11-14 16:00:03 -0800

    WIP: moved to vector

--- 2024/day09/part1.cpp
+++ 2024/day09/part1.cpp
@@ -7,10 +7,10 @@ using namespace std;
 
 void read_file(string& str)
 {
-  ifstream file("input");
+  ifstream file("test_input2");
   getline(file, str);
 }
-void write_raw_disk(string& disk_map, string& raw_disk)
+void write_raw_disk(string& disk_map, vector<int>& raw_disk)
 {
   int id = 0;
 
@@ -20,43 +20,43 @@ void write_raw_disk(string& disk_map, string& raw_disk)
     int blank_space = i+1 < disk_map.length() ? disk_map[i+1] - '0' : 0;  
 
     for(int j = 0; j < file_size; j++)
-      raw_disk += to_string(id);
+      raw_disk.push_back(id);
 
     for(int j = 0; j < blank_space; j++)
-      raw_disk += '.';
+      raw_disk.push_back(-1);
 
     id++;
   }
 }
 
-void defrag(string& disk)
+void defrag(vector<int> disk)
 {
   int leftIndx = 0;
-  int rightIndx = disk.length() - 1;
+  int rightIndx = disk.size() - 1;
 
   while( leftIndx < rightIndx )
   {
-    while( leftIndx < rightIndx && disk[leftIndx] != '.' )
+    while( leftIndx < rightIndx && disk[leftIndx] != -1 )
       leftIndx++;
     
-    while ( rightIndx > leftIndx && disk[rightIndx] == '.' )
+    while ( rightIndx > leftIndx && disk[rightIndx] == -1 )
       rightIndx--;
 
     disk[leftIndx] = disk[rightIndx];
-    disk[rightIndx] = '.';
+    disk[rightIndx] = -1;
     //leftIndx++; rightIndx--;
   }
 }
 
-unsigned long long int find_checksum(string& disk)
+unsigned long long int find_checksum(vector<int> disk)
 {
   unsigned long long int checksum = 0;
-  for(unsigned long long int i = 0; i < disk.length(); i++)
+  for(unsigned long long int i = 0; i < disk.size(); i++)
   {
-    if(disk[i] == '.')
+    if(disk[i] == -1)
       continue;
 
-    checksum += (disk[i] - '0') * i;
+    checksum += disk[i] * i;
   }
 
   return checksum;
@@ -67,7 +67,7 @@ int main()
   string disk_map = "";
   read_file(disk_map);
    
-  string raw_disk = "";
+  vector<int> raw_disk;
   write_raw_disk(disk_map , raw_disk);
 
   defrag(raw_disk);
